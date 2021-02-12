ac494628b10bd3411569582430ebd1ade4f5789a
SigmaTheFox <28538408+SigmaTheFox@users.noreply.github.com>
2021-02-12 12:18:34 +0100

    fixed avatar command sending low resolution URL

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,8 +4,8 @@ module.exports = {
     description: "Get someone else's or your own profile picture",
     execute(ayanami, message, args) {
         if (message.mentions.users.first()) {
-            return message.reply(message.mentions.users.first().displayAvatarURL())
+            return message.reply(message.mentions.users.first().displayAvatarURL({format: "png", dynamic: true, size: 4096}))
         }
-        else message.reply(message.author.displayAvatarURL())
+        else message.reply(message.author.displayAvatarURL({format: "png", dynamic: true, size: 4096}))
     }
 }
