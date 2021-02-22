3e8ffe4e4dd6a4594abc0417f3cb10fbcedc72c2
SigmaTheFox <28538408+SigmaTheFox@users.noreply.github.com>
2021-02-22 10:57:09 +0100

    fixed check if arg got entered

--- commands/reload.js
+++ commands/reload.js
@@ -7,11 +7,10 @@ module.exports = {
   args: true,
   usage: `<reload/load> <command name>`,
   execute(ayanami, message, args) {
-    if (message.author.id !== config.ownerID)
-      return message.reply("You're not my owner!");
-    if (!args || args.size < 1) return message.reply("Commander... You didn't tell me what to reload.");
+    if (message.author.id !== config.ownerID) return message.reply("You're not my owner!");
 
     if (/\bl(oad)?\b/gi.test(args[0])) {
+      if (!args[1]) return message.reply("Commander... Please specify a command to load.");
       if (ayanami.commands.has(args[1])) return message.reply("This command is already loaded.");
       try {
         let props = require(`./${args[1]}.js`);
@@ -22,6 +21,7 @@ module.exports = {
       }
     }
     else if (/\br(eload)?\b/gi.test(args[0])) {
+      if (!args[1]) return message.reply("Commander... Please specify a command to reload.")
       if (!ayanami.commands.has(args[1])) return message.reply("This command does not exist.");
   
       // The path is relative to the current folder.
