b43180f4df78a70470828eaebf7ff7cc99101e87
SigmaTheFox <28538408+SigmaTheFox@users.noreply.github.com>
2021-04-02 21:33:10 +0200

    ban command now deletes user from cache

--- commands/ban.js
+++ commands/ban.js
@@ -9,7 +9,7 @@ module.exports = {
     usage: "<@User> <reason>",
     execute(ayanami, message, args) {
         if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply("You don't have permission to use this command.")
-        
+
         let target = message.mentions.members.first();
         let reason = args.splice(1, args.length).join(" ");
         let channel = ayanami.channels.cache.get("783733205114421309");
@@ -22,12 +22,12 @@ module.exports = {
             .setTimestamp()
 
         message.guild.members.ban(target, { reason: reason })
-        .then(ayanami.users.cache.delete(target.id))
-        .catch(e => {
-            logger.error(e);
-            console.error(e);
-            return message.reply(`Failed to ban user ${target.user.tag}`);
-        })
+            .then(ayanami.users.cache.delete(target.id))
+            .catch(e => {
+                logger.error(e);
+                console.error(e);
+                return message.reply(`Failed to ban user ${target.user.tag}`);
+            })
         channel.send(embed)
     }
 }
