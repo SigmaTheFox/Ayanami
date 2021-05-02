96e50de9a062d4fd26896ab1acd418d82766739e
SigmaTheFox <28538408+SigmaTheFox@users.noreply.github.com>
2021-05-02 13:28:17 +0200

    help cleanup

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,6 @@
 const { Client, Message, MessageEmbed } = require("discord.js");
 const { prefix } = require("../settings/config.json");
 
-
 module.exports = {
   name: `help`,
   aliases: [`commands`],
@@ -16,53 +15,47 @@ module.exports = {
   execute(ayanami, message, args) {
     let embeds = {}, counts = {};
 
-    try {
-      ayanami.commands.forEach(cmd => {
-        if (!embeds[cmd.category]) {
-          embeds[cmd.category] = []
-          counts[cmd.category] = 0
-        }
-        if (!embeds[cmd.category][Math.floor(counts[cmd.category] / 25)]) {
-          embeds[cmd.category].push(new MessageEmbed()
-            .setTitle(`${cmd.category.toUpperCase()} ${Math.floor(counts[cmd.category] / 25) == 0 ? "" : Math.floor(counts[cmd.category] / 25) + 1}`)
-            .setAuthor(ayanami.user.username, ayanami.user.displayAvatarURL())
-            .setColor(45055)
-          )
-        }
-        embeds[cmd.category][Math.floor(counts[cmd.category] / 25)].addField(cmd.name, cmd.description ? cmd.description : "\u200b", true)
-        counts[cmd.category]++
-      })
-
-      if (args.length) {
-        let commandName = args[0].toLowerCase(),
-          cmd = ayanami.commands.get(commandName)
-            || ayanami.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-        if (!cmd) return message.reply(`There is no command called **${commandName}**.`);
-
-        let embed = new MessageEmbed()
-          .setTitle(`${cmd.name.toUpperCase()} HELP`)
+    ayanami.commands.forEach(cmd => {
+      if (!embeds[cmd.category]) { // Sort all commands by their category
+        embeds[cmd.category] = []
+        counts[cmd.category] = 0
+      } // If a category's embed has more than 25 fields, create a new embed
+      if (!embeds[cmd.category][Math.floor(counts[cmd.category] / 25)]) {
+        embeds[cmd.category].push(new MessageEmbed()
+          .setTitle(`${cmd.category.toUpperCase()} ${Math.floor(counts[cmd.category] / 25) == 0 ? "" : Math.floor(counts[cmd.category] / 25) + 1}`)
           .setAuthor(ayanami.user.username, ayanami.user.displayAvatarURL())
           .setColor(45055)
-        if (cmd.aliases) embed.addField("Aliases", cmd.aliases.join(", "));
-        if (cmd.description) embed.addField("Description", cmd.description);
-        if (cmd.usage) embed.addField("Usage", `${prefix}${cmd.name} ${cmd.usage}`)
+        )
+      } // Otherwise add more fields and increase the field count for that category
+      embeds[cmd.category][Math.floor(counts[cmd.category] / 25)].addField(cmd.name, cmd.description ? cmd.description : "\u200b", true)
+      counts[cmd.category]++
+    })
 
-        if (message.channel.type !== "dm") message.channel.send("Ok Commander. Check your DMs.")
-        return message.author.send(embed).catch(() => message.reply("Your DMs seem to be blocked."))
-      }
+    if (args.length) { // Check if the user specified a command to get help for
+      let commandName = args[0].toLowerCase(),
+        cmd = ayanami.commands.get(commandName)
+          || ayanami.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-      if (message.channel.type !== "dm") message.channel.send("Ok Commander. Check your DMs.\n*In case you're not getting anything... Check if you have DMs allowed in the server privacy settings*")
-      for (const key of Object.keys(embeds)) {
-        embeds[key].forEach(e => {
-          message.author.send(e)
-            .catch(() => { return })
-        })
-      }
-    } catch (error) {
-      message.channel.send("There was an error.");
-      console.error(error);
-      return  ayanami.logger.error(error);
+      if (!cmd) return message.reply(`There is no command called **${commandName}**.`);
+
+      let embed = new MessageEmbed()
+        .setTitle(`${cmd.name.toUpperCase()} HELP`)
+        .setAuthor(ayanami.user.username, ayanami.user.displayAvatarURL())
+        .setColor(45055)
+      if (cmd.aliases) embed.addField("Aliases", cmd.aliases.join(", "));
+      if (cmd.description) embed.addField("Description", cmd.description);
+      if (cmd.usage) embed.addField("Usage", `${prefix}${cmd.name} ${cmd.usage}`)
+
+      if (message.channel.type !== "dm") message.channel.send("Ok Commander. Check your DMs.")
+      return message.author.send(embed).catch(() => message.reply("Your DMs seem to be blocked."))
+    }
+
+    if (message.channel.type !== "dm") message.channel.send("Ok Commander. Check your DMs.\n*In case you're not getting anything... Check if you have DMs allowed in the server privacy settings*")
+    for (const key of Object.keys(embeds)) { // Loop through all embeds and send them one by one
+      embeds[key].forEach(e => {
+        message.author.send(e)
+          .catch(() => { return })
+      })
     }
   }
-};
+}
