bb3f96a904a9fe70a12f6375f4fb23439cecf821
SigmaTheFox <28538408+SigmaTheFox@users.noreply.github.com>
2021-05-03 13:37:27 +0200

    Added help menu for help command
    * Help menu added to help command

--- commands/help.js
+++ commands/help.js
@@ -1,37 +1,91 @@
 const { Client, Message, MessageEmbed } = require("discord.js");
 const { prefix } = require("../settings/config.json");
+const { Menu } = require("discord.js-menu");
+
+function DMCommandList(client, channel) {
+  let content = [];
+
+  content.push("Here are all my commands, Commander:");
+  content.push("```JSON\n" + client.commands.map(cmd => cmd.name).join(", ") + "```");
+  content.push(`To get further information about a command, you can use \`${prefix}help [command name]!\``);
+
+  channel.send(content, { split: true })
+}
+
+function sortCommands(client, author) {
+  let embeds = {}, fieldCounts = {}, embedCount = -1;
+
+  client.commands.forEach(cmd => {
+    // Create the category in the embeds and counts objects
+    if (!embeds[cmd.category]) {
+      embeds[cmd.category] = []
+      fieldCounts[cmd.category] = 0
+    }
+    // If a category's embed has more than 25 fields, create a new embed
+    if (!embeds[cmd.category][Math.floor(fieldCounts[cmd.category] / 25)]) {
+      embeds[cmd.category].push(new MessageEmbed()
+        .setTitle(`${cmd.category.toUpperCase()} ${Math.floor(fieldCounts[cmd.category] / 25) == 0 ? "" : Math.floor(fieldCounts[cmd.category] / 25) + 1}`)
+        .setAuthor(author.username, author.displayAvatarURL())
+        .setColor(45055)
+      )
+      embedCount++
+    } // Otherwise add more fields and increase the field count for that category
+    embeds[cmd.category][Math.floor(fieldCounts[cmd.category] / 25)].addField(cmd.name, cmd.description ? cmd.description : "\u200b", true)
+    fieldCounts[cmd.category]++
+  })
+
+  return {
+    embeds, embedCount
+  }
+}
+
+function createMenuPagesArray(embeds, embedCount) {
+  let menuPages = [], name = 0;
+  // Loop through all categories
+  for (let key of Object.keys(embeds)) {
+    // Loop through all embeds in the category
+    embeds[key].forEach(content => {
+      // Add the embed to the help menu array
+      switch (name) {
+        case 0:
+          menuPages.push({
+            name, content,
+            reactions: { "❎": "delete", "➡️": "next" }
+          })
+          name++
+          break;
+        case embedCount:
+          menuPages.push({
+            name, content,
+            reactions: { "❎": "delete", "⬅️": "previous" }
+          })
+          break;
+        default:
+          menuPages.push({
+            name, content,
+            reactions: { "❎": "delete", "⬅️": "previous", "➡️": "next" }
+          })
+          name++
+          break;
+      }
+    })
+  }
+  return menuPages
+}
 
 module.exports = {
   name: `help`,
   aliases: [`commands`],
   category: "utility",
   description: "This command",
-  usage: "(command name)",
+  usage: "[command name]",
   /**
    * @param {Client} ayanami 
    * @param {Message} message 
    * @param {String[]} args 
    */
   execute(ayanami, message, args) {
-    let embeds = {}, counts = {};
-
-    ayanami.commands.forEach(cmd => {
-      if (!embeds[cmd.category]) { // Sort all commands by their category
-        embeds[cmd.category] = []
-        counts[cmd.category] = 0
-      } // If a category's embed has more than 25 fields, create a new embed
-      if (!embeds[cmd.category][Math.floor(counts[cmd.category] / 25)]) {
-        embeds[cmd.category].push(new MessageEmbed()
-          .setTitle(`${cmd.category.toUpperCase()} ${Math.floor(counts[cmd.category] / 25) == 0 ? "" : Math.floor(counts[cmd.category] / 25) + 1}`)
-          .setAuthor(ayanami.user.username, ayanami.user.displayAvatarURL())
-          .setColor(45055)
-        )
-      } // Otherwise add more fields and increase the field count for that category
-      embeds[cmd.category][Math.floor(counts[cmd.category] / 25)].addField(cmd.name, cmd.description ? cmd.description : "\u200b", true)
-      counts[cmd.category]++
-    })
-
-    if (args.length) { // Check if the user specified a command to get help for
+    if (args.length) { // Send command specific help
       let commandName = args[0].toLowerCase(),
         cmd = ayanami.commands.get(commandName)
           || ayanami.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
@@ -46,16 +100,19 @@ module.exports = {
       if (cmd.description) embed.addField("Description", cmd.description);
       if (cmd.usage) embed.addField("Usage", `${prefix}${cmd.name} ${cmd.usage}`)
 
-      if (message.channel.type !== "dm") message.channel.send("Ok Commander. Check your DMs.")
-      return message.author.send(embed).catch(() => message.reply("Your DMs seem to be blocked."))
+      return message.author.send({ embed })
+        .then(() => {
+          if (message.channel.type === "dm") return;
+          message.channel.send("Ok Commander. Check your DMs.")
+        })
+        .catch(() => message.reply("Your DMs seem to be blocked."))
     }
 
-    if (message.channel.type !== "dm") message.channel.send("Ok Commander. Check your DMs.\n*In case you're not getting anything... Check if you have DMs allowed in the server privacy settings*")
-    for (const key of Object.keys(embeds)) { // Loop through all embeds and send them one by one
-      embeds[key].forEach(e => {
-        message.author.send(e)
-          .catch(() => { return })
-      })
-    }
+    if (message.channel.type === "dm") return DMCommandList(ayanami, message.channel)
+
+    let { embeds, embedCount } = sortCommands(ayanami, message.author)
+    let helpMenu = new Menu(message.channel, message.author.id, createMenuPagesArray(embeds, embedCount), 300000)
+    helpMenu.start();
+    message.delete();
   }
 }
