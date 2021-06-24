83d6887f2a6239e5e78707020a7eea0b7e717a76
SigmaTheFox <28538408+SigmaTheFox@users.noreply.github.com>
2021-06-24 14:17:41 +0200

    added character limit to translate cmd

--- commands/translate.js
+++ commands/translate.js
@@ -10,14 +10,17 @@ module.exports = {
     usage: "<Sentence>",
     async execute(ayanami, message, args) {
         let input = args.join(" ");
+        if (input.length > 1024) return message.channel.send("The text to translate can't be longer than 1024 characters.");
+        
         try {
             let { text } = await translatte(input);
+            if (text.length > 1024) return message.channel.send("The translated text is longer than 1024 characters.\nYou might want to use https://translate.google.com");
 
             const embed = new MessageEmbed()
                 .setColor(45055)
                 .addField("Input Sentence", input)
                 .addField("Translation", text);
-                
+
             return message.channel.send({ embed: embed });
         } catch (err) {
             return message.channel.send("There was an error while translating. Try again later.");
