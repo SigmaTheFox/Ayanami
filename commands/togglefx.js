181e538de856d80dfb0908644e6b7d7aa304ec43
SigmaTheFox <28538408+SigmaTheFox@users.noreply.github.com>
2022-10-03 11:14:39 +0200

    added togglefx

---
+++ commands/togglefx.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder, Client, CommandInteraction } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("togglefx")
+        .setDescription("Toggle fxtwitter on/off"),
+    /**
+     * @param {Client} ayanami
+     * @param {CommandInteraction} interaction
+     */
+    async execute(ayanami, interaction) {
+        let member = interaction.member,
+            findRole = interaction.guild.roles.cache.find(r => r.name === "fxtwitter");
+
+        if (!member.roles.cache.has(findRole.id)) {
+            member.roles.add(findRole);
+            return interaction.reply({ content: "Fxtwitter is now Enabled", ephemeral: true });
+        }
+        else if (member.roles.cache.has(findRole.id)) {
+            member.roles.remove(findRole);
+            return interaction.reply({ content: "Fxtwitter is now Disabled", ephemeral: true });
+        }
+    }
+}
