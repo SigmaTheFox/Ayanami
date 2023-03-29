3b13ba90e96f5803b3d66c4caae56a8b9249bfa8
SigmaTheFox <28538408+SigmaTheFox@users.noreply.github.com>
2023-03-29 10:10:29 +0200

    marked gelbooru as nsfw

--- commands/gelbooru.js
+++ commands/gelbooru.js
@@ -1,5 +1,5 @@
 const Booru = require("../modules/booru.js");
-const { SlashCommandBuilder, EmbedBuilder, Client, CommandInteraction, ChannelType } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, Client, CommandInteraction } = require("discord.js");
 const booru = new Booru(require("../settings/config.json").GBKey);
 
 let tags = ["-loli*", "-shota*", "-young", "-gore", "-amputee", "-furry", "-anthro"];
@@ -20,18 +20,15 @@ module.exports = {
         .setDescription("Search for an image using tags")
         .addStringOption(opt =>
             opt.setName("tags")
-                .setDescription("The tags to search for (use _ for multi-word tags)")),
+                .setDescription("The tags to search for (use _ for multi-word tags)"))
+        .setNSFW(true),
     /**
      * @param {Client} ayanami
      * @param {CommandInteraction} interaction
      */
     async execute(ayanami, interaction) {
-        if (!interaction.channel.nsfw && interaction.channel.type !== ChannelType.DM) {
-            return interaction.reply({ content: "Commander, please go to the NSFW channels or DMs for this.", ephemeral: true });
-        }
-
         let intString = interaction.options.getString("tags");
-
+        
         if (intString) {
             if (/\bLOLI\b/gi.test(intString) || /\bGORE\b/gi.test(intString) || /\bSHOTA\b/gi.test(intString)) {
                 return interaction.reply({ content: "… I don't really want to come close to you, Commander. Lolis, Shota and Gore are not allowed...", ephemeral: true });
