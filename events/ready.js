cdb39d45763098818a1fb4619e757739d4644136
SigmaTheFox <28538408+SigmaTheFox@users.noreply.github.com>
2023-08-18 12:31:24 +0200

    added free games announcement

--- events/ready.js
+++ events/ready.js
@@ -1,16 +1,26 @@
-const cron = require('node-cron')
+const cron = require('node-cron');
 
 module.exports = (ayanami) => {
-  ayanami.channels.cache
-    .filter((c) => c.name === 'roles')
-    .each((channel) => channel.messages.fetch())
-  console.log(`Taste the power of the demon...!`)
-  ayanami.logger.trace(`Taste the power of the demon...!`)
+	ayanami.channels.cache
+		.filter((c) => c.name === 'roles')
+		.each((channel) => channel.messages.fetch());
+	console.log(`Taste the power of the demon...!`);
+	ayanami.logger.trace(`Taste the power of the demon...!`);
 
-  cron.schedule(
-    '0 0 * * *',
-    () => {
-      require('../modules/getBirthdays')(ayanami)
-    }
-  )
-}
+	cron.schedule('0 0 * * *', () => {
+		require('../modules/getBirthdays')(ayanami);
+	});
+
+	let free_games_channel = ayanami.channels.cache.filter(
+		(c) => c.name === 'free-games'
+	);
+	cron.schedule('15 15 * * *', () => {
+		free_games_channel.each((channel) => {
+			let free_games_role = channel.guild.roles.cache.find(
+				(r) => r.name.toLowerCase() === 'free games'
+			).id;
+
+			require('../modules/games')(channel, `<@&${free_games_role}>`);
+		});
+	});
+};
---
+++ modules/games.js
@@ -0,0 +1,51 @@
+const { warn } = require('console');
+
+module.exports = async (channel, role) => {
+	const fs = require('fs');
+	const fetch = require('node-fetch');
+	const homeDir = require('os').homedir();
+	const fileDir = `${homeDir}/.config/free-games`;
+
+	if (!fs.existsSync(`${homeDir}/.config/free-games`))
+		fs.mkdirSync(`${fileDir}`);
+	if (fs.existsSync(`${fileDir}/gameList.json`))
+		fs.renameSync(`${fileDir}/gameList.json`, `${fileDir}/gameList-old.json`);
+
+	fetch(
+		'https://proxy.gxcorner.games/content/free-games?_limit=50&_sort=order:ASC'
+	).then((res) => {
+		new Promise((resolve, reject) => {
+			const file = fs.createWriteStream(`${fileDir}/gameList.json`);
+			res.body.pipe(file);
+			res.body.on('end', () => resolve(listGames()));
+			file.on('error', reject);
+		});
+	});
+
+	const listGames = () => {
+		const freeGames = require(`${fileDir}/gameList.json`);
+		let freeGamesOld = [];
+		if (fs.existsSync(`${fileDir}/gameList-old.json`))
+			freeGamesOld = require(`${fileDir}/gameList-old.json`);
+
+		let msg = [];
+
+		for (game of freeGames) {
+			if (game.store?.id === 3 || game.store?.id == 4 || game.store?.id === 11)
+				continue;
+
+			if (
+				freeGamesOld.length !== 0 &&
+				freeGamesOld.some((g) => g.game.title === game.game.title)
+			)
+				continue;
+			else
+				msg.push(`* [${game.game.title}](${game.url}) (${game.store?.name})`);
+		}
+
+		if (msg.length === 0) return console.log('No new free games');
+
+		msg.unshift(role);
+		return channel.send(msg.join('\n'));
+	};
+};
