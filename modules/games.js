ffeddf080096c809ca0757c3f0b6502442ae5a13
Valerio D'Onghia <valerio.donghia@outlook.com>
2023-09-04 23:59:44 +0200

    added pretty colors to free games embed

--- modules/games.js
+++ modules/games.js
@@ -1,15 +1,18 @@
 module.exports = async (channels) => {
-	const { EmbedBuilder } = require('discord.js');
+	const { EmbedBuilder, ChannelType } = require('discord.js');
+	const ColorThief = require('colorthief');
 	const fs = require('fs');
 	const fetch = require('node-fetch');
 	const homeDir = require('os').homedir();
 	const fileDir = `${homeDir}/.config/free-games`;
 	const URL = 'https://gamerpower.com/api/giveaways?platform=pc';
 
+	// Create the free games directory and rename the old games list if it exists
 	if (!fs.existsSync(fileDir)) fs.mkdirSync(fileDir);
 	if (fs.existsSync(`${fileDir}/gameList.json`))
 		fs.renameSync(`${fileDir}/gameList.json`, `${fileDir}/gameList-old.json`);
 
+	// Fetch the new free games
 	fetch(URL).then((res) => {
 		new Promise((resolve, reject) => {
 			const file = fs.createWriteStream(`${fileDir}/gameList.json`);
@@ -19,10 +22,11 @@ module.exports = async (channels) => {
 		});
 	});
 
-	const listGames = () => {
+	const listGames = async () => {
 		let freeGames = JSON.parse(fs.readFileSync(`${fileDir}/gameList.json`));
 		let freeGamesOld = [];
 
+		// If it exists, parse the old games list
 		if (fs.existsSync(`${fileDir}/gameList-old.json`)) {
 			freeGamesOld = JSON.parse(
 				fs.readFileSync(`${fileDir}/gameList-old.json`)
@@ -31,7 +35,10 @@ module.exports = async (channels) => {
 
 		let game_embeds = [];
 
+		// Loop through all games in the new games list.
 		for (game of freeGames) {
+			// If the embed array has 5 embeds in it, stop the loop.
+			// If instead the old games list is empty or the current new game's ID corresponds to the ID of an old game, skip the loop
 			if (game_embeds.length === 5) break;
 
 			if (
@@ -39,12 +46,17 @@ module.exports = async (channels) => {
 				freeGamesOld.some((g) => g.id === game.id)
 			)
 				continue;
-			else
+			else {
+				// Get the dominant color of the game's image
+				// And add it alongside other deal info to an embed
+				let color = await ColorThief.getColor(game.image);
+
 				game_embeds.push(
 					new EmbedBuilder()
 						.setTitle(game.title)
 						.setURL(game['open_giveaway'])
 						.setImage(game.image)
+						.setColor(color)
 						.setDescription(game.description)
 						.addFields([
 							{ name: 'Type', value: game.type },
@@ -53,10 +65,13 @@ module.exports = async (channels) => {
 							{ name: 'Instructions', value: game.instructions },
 						])
 				);
+			}
 		}
 
 		if (game_embeds.length === 0) return console.log('No new free games');
 
+		// Loop through all channels passed to the file and find the corresponding channel's server's free games role
+		// Afterwards send the embeds to the free games channel, and if it's an announcement channel, crosspost it
 		channels.each(async (channel) => {
 			let free_games_role = channel.guild.roles.cache.find(
 				(r) => r.name.toLowerCase() === 'free games'
@@ -66,7 +81,8 @@ module.exports = async (channels) => {
 				content: `<@&${free_games_role}>`,
 				embeds: game_embeds,
 			});
-			msg.crosspost();
+
+			if (msg.channel.type === ChannelType.GuildAnnouncement) msg.crosspost();
 		});
 	};
 };
