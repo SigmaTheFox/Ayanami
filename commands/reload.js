cdd6082f9eeeadb22993001e6582df3e0c901cc9
SigmaTheFox <valerio.donghia@outlook.com>
2024-03-15 13:54:17 +0100

    added reload command

---
+++ commands/reload.js
@@ -0,0 +1,45 @@
+const {
+	SlashCommandBuilder,
+	Client,
+	CommandInteraction,
+	PermissionFlagsBits,
+} = require('discord.js');
+
+module.exports = {
+	private: true,
+	data: new SlashCommandBuilder()
+		.setName('reload')
+		.setDescription('Reloads a command')
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+		.addStringOption(opt =>
+			opt.setName('command').setDescription('The command to reload').setRequired(true)
+		),
+	/**
+	 *
+	 * @param {Client} ayanami
+	 * @param {CommandInteraction} interaction
+	 */
+	async execute(ayanami, interaction) {
+		const commandName = interaction.options.getString('command', true).toLowerCase();
+		const command = interaction.client.commands.get(commandName);
+
+		if (!command) {
+			return interaction.reply(`There is no command with name \`${commandName}\`!`);
+		}
+
+		delete require.cache[require.resolve(`./${command.data.name}.js`)];
+
+		try {
+			interaction.client.commands.delete(command.data.name);
+			const newCommand = require(`./${command.data.name}.js`);
+			interaction.client.commands.set(newCommand.data.name, newCommand);
+			await interaction.reply(`Command \`${newCommand.data.name}\` was reloaded!`);
+		} catch (err) {
+			console.error(err);
+			ayanami.logger.error(err);
+			await interaction.reply(
+				`There was an error while reloading a command \`${command.data.name}\`:\n\`${err.message}\``
+			);
+		}
+	},
+};
--- deploy-commands.js
+++ deploy-commands.js
@@ -1,26 +1,34 @@
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord.js");
-const { clientID, guildID, token } = require("./settings/config.json");
-const fs = require("fs");
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord.js');
+const { clientID, guildID, privateGuildID, token } = require('./settings/config.json');
+const fs = require('fs');
 
 let globalCommands = [];
 let guildCommands = [];
-const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
+let privateCommands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	if (command.global === true) globalCommands.push(command.data.toJSON());
 	else if (command.global === false) guildCommands.push(command.data.toJSON());
+	else if (command.private) privateCommands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: "10" }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(token);
 
 rest.put(Routes.applicationCommands(clientID), { body: globalCommands })
-	.then(() => console.log("Successfully registered global commands."))
+	.then(() => console.log('Successfully registered global commands.'))
 	.catch(console.error);
 
 rest.put(Routes.applicationGuildCommands(clientID, guildID), {
 	body: guildCommands,
 })
-	.then(() => console.log("Successfully registered guild commands."))
+	.then(() => console.log('Successfully registered guild commands.'))
+	.catch(console.error);
+
+rest.put(Routes.applicationGuildCommands(clientID, privateGuildID), {
+	body: privateCommands,
+})
+	.then(() => console.log('Successfully registered private commands.'))
 	.catch(console.error);
