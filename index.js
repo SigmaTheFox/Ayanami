630f9092dd81a2caabc35f4c2b54e2001b4105f4
SigmaTheFox <valerio.donghia@outlook.com>
2024-03-27 13:43:13 +0100

    set status activity to custom

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const { Client, Options, Collection, GatewayIntentBits, Partials } = require("discord.js");
-const fs = require("fs");
-const config = require("./settings/config.json");
+const {
+	Client,
+	Options,
+	Collection,
+	GatewayIntentBits,
+	Partials,
+	ActivityType,
+} = require('discord.js');
+const fs = require('fs');
+const config = require('./settings/config.json');
 
 let intents = [
 	GatewayIntentBits.Guilds,
@@ -17,7 +24,8 @@ const ayanami = new Client({
 	presence: {
 		activities: [
 			{
-				name: "Real life is a shit-tier game",
+				type: ActivityType.Custom,
+				name: 'Real life is a shit-tier game',
 			},
 		],
 	},
@@ -28,37 +36,37 @@ const ayanami = new Client({
 	}),
 });
 
-ayanami.logger = require("./modules/logger");
+ayanami.logger = require('./modules/logger');
 ayanami.commands = new Collection();
 ayanami.msgCommands = new Collection();
 
 // Handle SlashCommands
-const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
 	let command = require(`./commands/${file}`);
 	ayanami.commands.set(command.data.name, command);
 }
 
 // Handle Message Commands
-const msgCommandFiles = fs.readdirSync("./msgCommands").filter((file) => file.endsWith(".js"));
+const msgCommandFiles = fs.readdirSync('./msgCommands').filter(file => file.endsWith('.js'));
 for (const file of msgCommandFiles) {
 	let command = require(`./msgCommands/${file}`);
 	ayanami.msgCommands.set(command.name, command);
 }
 
 // Require and attach each event file to the appropriate event
-const eventFiles = fs.readdirSync("./events").filter((file) => file.endsWith(".js"));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
 	let event = require(`./events/${file}`);
-	let eventName = file.split(".")[0];
+	let eventName = file.split('.')[0];
 
 	// Load events and require the client name before the event outputs
-	if (eventName !== "ready") ayanami.on(eventName, event.bind(null, ayanami));
+	if (eventName !== 'ready') ayanami.on(eventName, event.bind(null, ayanami));
 	else ayanami.once(eventName, event.bind(null, ayanami));
 	delete require.cache[require.resolve(`./events/${file}`)];
 }
 
-ayanami.login(config.token).catch((err) => {
+ayanami.login(config.token).catch(err => {
 	console.error(err);
 	ayanami.logger.error(err);
 });
