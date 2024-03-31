4b75a646c3eae6a54b70c9a7f58a54b03ef03c0f
SigmaTheFox <valerio.donghia@outlook.com>
2024-03-31 13:59:26 +0200

    removed source mask from source command

--- commands/source.js
+++ commands/source.js
@@ -1,9 +1,10 @@
-const { SlashCommandBuilder, Client, CommandInteraction, EmbedBuilder } = require("discord.js");
-const sagiri = require("sagiri");
-const sagiriClient = sagiri(require("../settings/config.json").sauceKey);
+const { SlashCommandBuilder, Client, CommandInteraction, EmbedBuilder } = require('discord.js');
+const sagiri = require('sagiri');
+const sagiriClient = sagiri(require('../settings/config.json').sauceKey);
 
 async function getSource(client, imgURL) {
-	if (!imgURL.includes("http://") && !imgURL.includes("https://")) return "Please enter a valid URL";
+	if (!imgURL.includes('http://') && !imgURL.includes('https://'))
+		return 'Please enter a valid URL';
 
 	let min_sim = 60,
 		filteredResults,
@@ -13,19 +14,21 @@ async function getSource(client, imgURL) {
 		};
 
 	try {
-		let results = await sagiriClient(imgURL, {
+		let results = await sagiriClient(
+			imgURL /*, {
 			mask: [5, 9, 12, 16, 25, 26, 27, 29, 34, 41],
-		});
+		}*/
+		);
 
-		filteredResults = results.filter((r) => r.similarity >= min_sim);
+		filteredResults = results.filter(r => r.similarity >= min_sim);
 	} catch (err) {
 		console.error(err);
 		client.logger.error(err);
-		return "There was an error";
+		return 'There was an error';
 	}
 
 	if (!filteredResults || filteredResults.length === 0) {
-		output.results.push({ name: "Nothing Found", value: "\u200b" });
+		output.results.push({ name: 'Nothing Found', value: '\u200b' });
 	} else {
 		for (item of filteredResults) {
 			if (output.results.length === 25) break;
@@ -45,7 +48,7 @@ function sendEmbed(interaction, source, imgURL) {
 		.setColor(45055)
 		.addFields(source.results)
 		.setThumbnail(imgURL)
-		.setFooter({ text: "Powered by Saucenao" });
+		.setFooter({ text: 'Powered by Saucenao' });
 
 	interaction.reply({ embeds: [embed] });
 }
@@ -55,22 +58,25 @@ let source;
 module.exports = {
 	global: true,
 	data: new SlashCommandBuilder()
-		.setName("source")
-		.setDescription("Get the source of an image")
-		.addSubcommand((cmd) =>
+		.setName('source')
+		.setDescription('Get the source of an image')
+		.addSubcommand(cmd =>
 			cmd
-				.setName("url")
-				.setDescription("Get the source of an image using a URL")
-				.addStringOption((opt) =>
-					opt.setName("link").setDescription("The direct URL to the image").setRequired(true)
+				.setName('url')
+				.setDescription('Get the source of an image using a URL')
+				.addStringOption(opt =>
+					opt
+						.setName('link')
+						.setDescription('The direct URL to the image')
+						.setRequired(true)
 				)
 		)
-		.addSubcommand((cmd) =>
+		.addSubcommand(cmd =>
 			cmd
-				.setName("attachment")
-				.setDescription("Get the source of an image using an attachment")
-				.addAttachmentOption((opt) =>
-					opt.setName("image").setDescription("The image attachment").setRequired(true)
+				.setName('attachment')
+				.setDescription('Get the source of an image using an attachment')
+				.addAttachmentOption(opt =>
+					opt.setName('image').setDescription('The image attachment').setRequired(true)
 				)
 		),
 	/**
@@ -78,14 +84,19 @@ module.exports = {
 	 * @param {CommandInteraction} interaction
 	 */
 	async execute(ayanami, interaction) {
-		if (interaction.options.getSubcommand() === "url") {
-			source = await getSource(ayanami, interaction.options.getString("link"));
-			if (typeof source === "string") return interaction.reply(source);
-			else return sendEmbed(interaction, source, interaction.options.getString("link"));
-		} else if (interaction.options.getSubcommand() === "attachment") {
-			source = await getSource(ayanami, interaction.options.getAttachment("image").url);
-			if (typeof source === "string") return interaction.reply(source);
-			else return sendEmbed(interaction, source, interaction.options.getAttachment("image").url);
+		if (interaction.options.getSubcommand() === 'url') {
+			source = await getSource(ayanami, interaction.options.getString('link'));
+			if (typeof source === 'string') return interaction.reply(source);
+			else return sendEmbed(interaction, source, interaction.options.getString('link'));
+		} else if (interaction.options.getSubcommand() === 'attachment') {
+			source = await getSource(ayanami, interaction.options.getAttachment('image').url);
+			if (typeof source === 'string') return interaction.reply(source);
+			else
+				return sendEmbed(
+					interaction,
+					source,
+					interaction.options.getAttachment('image').url
+				);
 		}
 	},
 };
