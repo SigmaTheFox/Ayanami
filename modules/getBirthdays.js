51d187585765f64c4a36880d425edbb0114158c8
SigmaTheFox <valerio.donghia@outlook.com>
2026-06-26 12:26:40 +0200

    fixed birthdayt?

--- modules/getBirthdays.js
+++ modules/getBirthdays.js
@@ -1,13 +1,13 @@
-const { Client } = require("discord.js");
-const { BirthdaysDB } = require("./dbObjects.js");
-const isValidDate = require("./isValidDate.js");
+const { Client } = require('discord.js');
+const { BirthdaysDB } = require('./dbObjects.js');
+const isValidDate = require('./isValidDate.js');
 
 /**
  * @param {Client} client
  **/
-module.exports = async (client) => {
+module.exports = async client => {
 	let todayDate = new Date();
-	let today = [todayDate.getUTCMonth() + 1, todayDate.getUTCDate()].join("-");
+	let today = [todayDate.getUTCMonth() + 1, todayDate.getUTCDate()].join('-');
 	let msg = [];
 
 	let birthdays = await BirthdaysDB.findAll({
@@ -15,32 +15,33 @@ module.exports = async (client) => {
 	});
 
 	let leaps = await BirthdaysDB.findAll({
-		where: { birthday: "2-29" },
+		where: { birthday: '2-29' },
 	});
 
 	if (birthdays.length) {
-		for (birthday of birthdays) {
-			let username = client.users.cache.get(birthday.get("discord_id")).username;
+		for (let birthday of birthdays) {
+			let userId = client.users.cache.get(birthday.get('discord_id'));
 
-			if (birthday.get("birthday") === today) msg.push(`Happy Birthday **${username}**!\n`);
+			if (birthday.get('birthday') === today) msg.push(`Happy Birthday ${userId}!\n`);
 		}
 	}
 
 	if (leaps.length) {
-		for (leap of leaps) {
-			let username = client.users.cache.get(leap.get("discord_id")).username;
+		for (let leap of leaps) {
+			let userId = client.users.cache.get(leap.get('discord_id'));
 
-			if (today === "2-28" && !isValidDate(29, 2, todayDate.getUTCFullYear())) {
-				msg.push(`Happy fake Birthday **${username}**!\n`);
+			if (today === '2-28' && !isValidDate(29, 2, todayDate.getUTCFullYear())) {
+				msg.push(`Happy fake Birthday ${userId}!\n`);
 				break;
 			}
 
-			if (today === "2-29") msg.push(`Happy leap Birthday **${username}**!\n`);
+			if (today === '2-29') msg.push(`Happy leap Birthday ${userId}!\n`);
 		}
 	}
 
-	if (msg.length)
-		client.channels.cache
-			.filter((c) => c.name === "birthdays")
-			.each((channel) => channel.send(msg.join("")));
+	if (msg.length) {
+		let birthdayChannels = client.channels.cache.filter(c => c.name === 'birthdays');
+
+		birthdayChannels.each(channel => channel.send(msg.join('\n')));
+	}
 };
